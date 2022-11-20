b88985f35413d95fb27f12c518ae6188c45cf9bd
Kent Wong <kent_wong@outlook.com>
2022-11-20 21:14:24 +1100

    feat: control endpoints separately

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 // status variables
-let userStatus;
-let weatherStatus;
-let itemStatus;
+let userStatus = 200;
+let weatherStatus = 200;
+let itemStatus = 200;
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
@@ -18,36 +18,70 @@ server.use(
 );
 
 server.post("/statusform", (req, res) => {
-  console.log("this is req", req.body.name);
-  userStatus = req.body?.userStatus;
-  weatherStatus = req.body?.weatherStatus;
-  itemStatus = req.body?.itemStatus;
+  console.log("this is req", req.body);
+  userStatus = req.body?.userStatus || userStatus;
+  weatherStatus = req.body?.weatherStatus || weatherStatus;
+  itemStatus = req.body?.itemStatus || itemStatus;
   res.status(200).jsonp({
     message: "status updated",
   });
+});
+
+server.get("/users", (req, res, next) => {
+  console.log("user request");
+  if (userStatus === "400") {
+    res.status(400).jsonp({
+      error: "400",
+    });
+  } else if (userStatus === "404") {
+    res.status(404).jsonp({
+      error: "404",
+    });
+  } else if (userStatus === "500") {
+    res.status(500).jsonp({
+      error: "500",
+    });
+  }
+  next();
+});
+
+server.get("/weather", (req, res, next) => {
+  console.log("weather request");
+  if (weatherStatus === "400") {
+    res.status(400).jsonp({
+      error: "400",
+    });
+  } else if (weatherStatus === "404") {
+    res.status(404).jsonp({
+      error: "404",
+    });
+  } else if (weatherStatus === "500") {
+    res.status(500).jsonp({
+      error: "500",
+    });
+  }
+  next();
+});
+
+server.get("/items", (req, res, next) => {
+  console.log("items request");
+  if (itemStatus === "400") {
+    res.status(400).jsonp({
+      error: "400",
+    });
+  } else if (itemStatus === "404") {
+    res.status(404).jsonp({
+      error: "404",
+    });
+  } else if (itemStatus === "500") {
+    res.status(500).jsonp({
+      error: "500",
+    });
+  }
+  next();
 });
 
 server.use(router);
 server.listen(5000, () => {
   console.log("JSON Server is running");
 });
-
-router.render = (req, res) => {
-  if (reqStatus === "200") {
-    res.status(200).jsonp({
-      error: "200",
-    });
-  } else if (reqStatus === "400") {
-    res.status(400).jsonp({
-      error: "400",
-    });
-  } else if (reqStatus === "404") {
-    res.status(404).jsonp({
-      error: "404",
-    });
-  } else if (reqStatus === "500") {
-    res.status(500).jsonp({
-      error: "500",
-    });
-  }
-};
