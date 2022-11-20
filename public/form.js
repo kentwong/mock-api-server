6c6458da77b80a3763581b3514c8220c6895264e
Kent Wong <kent_wong@outlook.com>
2022-11-20 17:46:45 +1100

    feat: beautify mock api server ui

--- public/form.js
+++ public/form.js
@@ -1,23 +1,51 @@
-const form = document.getElementById("contact-us-form");
+/**************************
+ * Alert for status update
+ **************************/
+const alertPlaceholder = document.getElementById("updateAlert");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+const alert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div><svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#${type}Svg"/></svg>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
 
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
-  //   formData.append("name", document.querySelector('input[name="name"]').value);
+  alertPlaceholder.append(wrapper);
+};
 
-  console.log(
-    "kent this is js",
-    document.querySelector('input[name="name"]').value
-  );
-  fetch("/testform", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => console.log(response.json()))
-    .catch((error) => console.log(error));
+/**************************
+ * Handle Form Submission
+ **************************/
+const forms = document.querySelectorAll(".statusForm");
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (e) => {
+    console.log("this is ew", e);
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    fetch("/statusform", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          alert("Woohoo, status updated!", "success");
+        } else {
+          alert("Something went wrong!", "danger");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Something went wrong!", "danger");
+      });
+  });
 });
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// status variables
+let userStatus;
+let weatherStatus;
+let itemStatus;
+
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(
@@ -12,18 +17,37 @@ server.use(
   })
 );
 
-server.post("/testform", (req, res) => {
+server.post("/statusform", (req, res) => {
   console.log("this is req", req.body.name);
-  res.send("this is done");
+  userStatus = req.body?.userStatus;
+  weatherStatus = req.body?.weatherStatus;
+  itemStatus = req.body?.itemStatus;
+  res.status(200).jsonp({
+    message: "status updated",
+  });
 });
 
 server.use(router);
-server.listen(3030, () => {
+server.listen(5000, () => {
   console.log("JSON Server is running");
 });
 
-// router.render = (req, res) => {
-//   res.status(500).jsonp({
-//     error: "error message here",
-//   });
-// };
+router.render = (req, res) => {
+  if (reqStatus === "200") {
+    res.status(200).jsonp({
+      error: "200",
+    });
+  } else if (reqStatus === "400") {
+    res.status(400).jsonp({
+      error: "400",
+    });
+  } else if (reqStatus === "404") {
+    res.status(404).jsonp({
+      error: "404",
+    });
+  } else if (reqStatus === "500") {
+    res.status(500).jsonp({
+      error: "500",
+    });
+  }
+};
